9173d882015aa358278f37fabfcf1496e8f176e2
zaber8787 <zaber8787@gmail.com>
2024-01-03 14:02:04 +0800

    folder

--- Hz.py
+++ Hz.py
@@ -7,26 +7,23 @@ from obspy.core.inventory import read_inventory
 from obspy.signal.trigger import classic_sta_lta
 import pandas as pd
 
-import obspy
-import pywt
-
 inv = read_inventory(r'C:\Users\Brian\Desktop\earthscience\output.dataless')
 
 
 # 讀取地震數據
-st = obspy.read(
-    r"C:\Users\Brian\Desktop\earthscience\data\2022-09-17 135708GDMSdata (1).mseed")
-
-st.detrend('linear')
-st.remove_response(inventory=inv, output='DISP')
-st.filter('bandpass', freqmin=2, freqmax=50)
-
-# 計算小波變換
-cwt = pywt.cwt(st[0].data, 'haar', widths=np.arange(1, 20, 1))
+path = r"C:\Users\Brian\Desktop\earthscience\data"
+files = os.listdir(path)
 
 
-# 計算 P 波到達時間
-p_arrival_time = np.argmax(cwt[0, :])
+def check(st):
+    st[2].detrend('linear')
+    st[2].remove_response(inventory=inv, output='DISP')
+    st[2].filter('bandpass', freqmin=2, freqmax=50)
+    st[2].plot()
 
-# 輸出 P 波到達時間
-print(p_arrival_time)
+
+for i in files:
+    ext = os.path.splitext(i)[-1]
+    if (ext == '.mseed'):
+        st = read(os.path.join(path, i))
+        check(st)
