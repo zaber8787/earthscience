b4633c86639a1e81972f646b1b2df2d016bc87fd
zaber8787 <zaber8787@gmail.com>
2024-07-24 23:56:56 +0800

    update

--- Hz.py
+++ Hz.py
@@ -1,28 +1,37 @@
-from obspy import read
+from obspy import read, UTCDateTime
 import numpy as np
 from scipy.fft import fft, fftfreq
+import json
+import os
 
-# 读取数据
-st = read(r'C:\Users\Brian\Desktop\earthscience\data\2022-09-17T13_43_40.mseed')
-tr = st[2]  # 选择其中一路地震信号
+with open('starttime.json', 'r', encoding='utf-8')as file:
+    f = json.load(file)
 
-# 预处理地震信号
-tr.detrend('linear')
-tr.filter('bandpass', freqmin=0.5, freqmax=5)
+tmp = f.keys()
+res = {}
+for i in tmp:
+    st = read("data/" + i)
+    tr = st[2]
 
-tr.trim(tr.stats.starttime + 175.3, tr.stats.starttime + 175.3 + 3)
+    # 预处理地震信号
+    tr.detrend('linear')
+    tr.filter('bandpass', freqmin=0.5, freqmax=5)
+    delta = UTCDateTime(f[i]) - UTCDateTime(tr.stats.starttime)
+    tr.trim(tr.stats.starttime + delta, tr.stats.starttime + delta + 3)
 
-# 获取信号数据并进行 FFT
-signal = tr.data
-spectrum = np.abs(fft(signal))
-frequencies = fftfreq(len(signal), d=tr.stats.delta)
+    # 获取信号数据并进行 FFT
+    signal = tr.data
+    spectrum = np.abs(fft(signal))
+    frequencies = fftfreq(len(signal), d=tr.stats.delta)
 
-# 找到频谱中能量最高的位置
-index_of_max_energy = np.argmax(spectrum)
-center_frequency = frequencies[index_of_max_energy]
+    # 找到频谱中能量最高的位置
+    index_of_max_energy = np.argmax(spectrum)
+    center_frequency = frequencies[index_of_max_energy]
+    res[i] = center_frequency
+    # 输出结果
+    print("中心频率:", center_frequency)
 
-# 输出结果
-print("中心频率:", center_frequency)
-
-# 绘制信号波形
-tr.plot(type='relative')
+    # 绘制信号波形
+    # tr.plot(type='relative')
+with open('result.json', 'w', encoding='utf-8')as file:
+    json.dump(res, file,indent=2,ensure_ascii=False)
--- view.py
+++ view.py
@@ -1,17 +1,29 @@
-import os 
+import os
 from obspy import read, UTCDateTime
 import numpy as np
 import matplotlib.pyplot as plt
 from obspy.core.inventory import read_inventory
 from obspy.signal.trigger import classic_sta_lta
 import pandas as pd
-
-inv = read_inventory("") # output data
-path = "" #file path
-
-st = read(path)
-st = st.detrend('linear')
-st = st.remove_response(inventory = inv,output = 'DISP')
-st = st.filter('bandpass', freqmin=0.5, freqmax=5)
-st = st.filter('bandpass', freqmin=0.5, freqmax=5)
-st[2].plot()
+import json
+file = os.listdir("data")
+for i in file:
+    with open('starttime.json', 'r', encoding='utf-8')as temp:
+        f = json.load(temp)
+    inv = read_inventory("output.stationXML")  # output data
+    path = i  # file path
+    st = read("data/" + path)
+    st = st.detrend('linear')
+    st = st.remove_response(inventory=inv, output='DISP')
+    st = st.filter('bandpass', freqmin=0.5, freqmax=5)
+    st = st.filter('bandpass', freqmin=0.5, freqmax=5)
+    st[2].plot()
+    while True:
+        tmp = input("格式:y-m-dTh:m:s")
+        print(tmp)
+        a = input("Is that the right time?")
+        if (a == 'y' or a == 'yes'):
+            f[i] = tmp
+            with open('starttime.json', 'w', encoding='utf-8')as b:
+                json.dump(f, b, indent=2, ensure_ascii=False)
+            break
